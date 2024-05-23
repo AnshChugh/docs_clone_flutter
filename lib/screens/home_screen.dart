af28a27374c697b01f38a0a0d1936fb9eabddd80
AnshChugh <Anshchugh20@GMAil.com>
2024-05-23 23:47:21 +0530

    added a skeleton document screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,29 @@
 import 'package:docs_clone_flutter/colors.dart';
 import 'package:docs_clone_flutter/repository/auth_repository.dart';
+import 'package:docs_clone_flutter/repository/document_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
   void signOut(WidgetRef ref) {
     ref.read(authRepositoryProvider).signOut();
-    ref.read(userProvider.notifier).update((state)=>null);
+    ref.read(userProvider.notifier).update((state) => null);
+  }
+
+  void createDocument(BuildContext context, WidgetRef ref) async {
+    String token = ref.read(userProvider)!.token;
+    final navigator = Routemaster.of(context);
+    final snackBar = ScaffoldMessenger.of(context);
+    final errorModel =
+        await ref.read(documentRepositoryProvider).createDocument(token);
+
+    if (errorModel.data != null) {
+      navigator.push('/document/${errorModel.data.id}');
+    } else {
+      snackBar.showSnackBar(SnackBar(content: Text(errorModel.error!)));
+    }
   }
 
   @override
@@ -18,7 +34,7 @@ class HomeScreen extends ConsumerWidget {
         elevation: 0,
         actions: [
           IconButton(
-              onPressed: () {},
+              onPressed: () => createDocument(context, ref),
               icon: const Icon(
                 Icons.add,
                 color: kBlackColor,
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const {password} = require('./secure_credentials');
 const {authRouter} = require('./routes/auth');
 // idk what is this for
 const cors = require('cors');
+const documentRouter = require('./routes/document');
 const PORT = process.env.PORT | 3001;
 const app = express();
 
@@ -15,6 +16,9 @@ app.use(cors());
 
 app.use(express.json());
 app.use(authRouter);
+app.use(documentRouter);
+
+
 mongoose.connect(DB).then(()=>{
     console.log('connection successful');
 }).catch((err)=>{
