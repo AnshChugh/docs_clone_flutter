f9683711022d9850786f3364673de45c2ff8f134
AnshChugh <Anshchugh20@GMAil.com>
2024-05-24 15:13:52 +0530

    setup UI for text editor and added flutter_quill package

--- lib/screens/document_screen.dart
+++ lib/screens/document_screen.dart
@@ -1,4 +1,6 @@
+import 'package:docs_clone_flutter/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart' as quill;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DocumentScreen extends ConsumerStatefulWidget {
@@ -10,12 +12,94 @@ class DocumentScreen extends ConsumerStatefulWidget {
 }
 
 class _DocumentScreenState extends ConsumerState<DocumentScreen> {
+  final titleController = TextEditingController(text: 'Untitled Document');
+  final quill.QuillController _controller = quill.QuillController.basic();
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(widget.id),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: kWhiteColor,
+          elevation: 0,
+          actions: [
+            Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.lock,
+                    size: 16,
+                    color: kWhiteColor,
+                  ),
+                  label: const Text(
+                    'Share',
+                    style: TextStyle(color: kWhiteColor),
+                  ),
+                  style: ElevatedButton.styleFrom(backgroundColor: kBlueColor),
+                )),
+          ],
+          title: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Row(
+              children: [
+                Image.asset(
+                  './assets/images/docs-logo.png',
+                  height: 40,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                SizedBox(
+                  width: 180,
+                  child: TextField(
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: kBlueColor)),
+                        contentPadding: EdgeInsets.only(left: 10)),
+                  ),
+                )
+              ],
+            ),
+          ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(11),
+            child: Container(
+              decoration: BoxDecoration(border: Border.all(color: kGreyColor)),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+              children: [
+                const SizedBox(height:10 ,),
+          quill.QuillToolbar.simple(configurations: 
+              quill.QuillSimpleToolbarConfigurations(controller: _controller)),
+          Expanded(
+              child: SizedBox(
+                width: 750,
+                child: Card(
+                  color: kWhiteColor,
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: quill.QuillEditor.basic(
+                    configurations: quill.QuillEditorConfigurations(controller: _controller),
+                    ),
+                  ),
+                ),
+              ),
+          )
+              ],
+          ),
+        ));
   }
 }
